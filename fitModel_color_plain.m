b5254109e4e2b5787333c2db48e57f31743aea33
Christian Kellner <gicmo@gnome.org>
2011-07-11 13:56:46 +0200

    Extract the image displaying code into its own separate function.

--- fitModel_color_plain.m
+++ fitModel_color_plain.m
@@ -25,37 +25,7 @@ Result.images = prepare_images (dataPar);
 % to the user for visual validation
 
 if dataPar.doDebug
-    figure (1);
-    set (0, 'CurrentFigure', 1); 
-    colormap (gray)
-    
-    nx = length (Result.images);
-    ny = dataPar.dataDim;
-    ha = tight_subplot (nx, ny, [.01 .03], [.01 .01]); 
-    
-    n_plots = ny * nx;
-    
-    refxs = [1, 3, 3, 1, 1];
-    refys = [2, 2, 4, 4, 2];
-    
-    for idx=1:n_plots
-        
-        n_img = floor ((idx - 1) / ny) + 1;
-        n_p = mod (idx  - 1, ny) + 1;
-        img = Result.images(n_img).imgData;
-        refkoos = Result.images(n_img).refkoos;
-        bf = squeeze (img(n_p, :, :));
-        set (gcf, 'CurrentAxes', ha(idx));
-        %title (num2str (idx));
-        hold on
-        imagesc(bf');
-        axis image;
-        axis off;
-        plot (refkoos(refxs), refkoos(refys), 'r-');
-        rotate (ha(idx), [1 0 0], 180);
-    end
-    
-    drawnow;
+    displayImages (Result, dataPar, 1);
 end
 
 [L,M] = size(Model.A);
