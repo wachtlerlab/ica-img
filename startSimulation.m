c35df9d82dba03df008522766e5c408ad839c2d0
Christian Kellner <gicmo@gnome.org>
2011-07-13 19:20:27 +0200

    Small change sto startSimulation

--- startSimulation.m
+++ startSimulation.m
@@ -1,6 +1,7 @@
 if exist ('modelId', 'var') == 0
-  modelId = 'color_plain';
+  modelId = 'color_cs_rect_1';
 end
 
+fprintf ('Starting simulation for %s', modelId)
 [Model, Result] = fitModel (modelId);
 
