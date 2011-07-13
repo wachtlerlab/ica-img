05fa1544e93f4c5d7a7a81d65fc3cae6afdae30d
Christian Kellner <gicmo@gnome.org>
2011-07-13 19:20:48 +0200

    New function to plot Model.A

---
+++ plotModel.m
@@ -0,0 +1,51 @@
+function [hf] = plotModel (Model, start, num)
+
+if (nargin < 3)
+  num = 10;
+end;
+
+if (nargin < 2)
+  start = 0;
+else
+  start = start - 1;
+end
+
+patchSize = Model.patchSize;
+dataDim = Model.dataDim;
+
+[~,M] = size(Model.A);
+A = sortAbf (Model.A);
+
+hf = figure ('Name', ['Basis Functions: ', Model.name], ...
+  'Position', [0, 0, 800, 1000], 'Color', 'w', 'PaperType', 'A4');
+
+ha = tight_subplot (num, dataDim, [.01 .01], 0);
+
+lblChan = {{'S (off)'}, {'S (on)'}, {'M (off)'}, {'M (on)'}, {'L (off)'}, {'L (on)'}};
+
+for ii = 1:num
+  
+  idx = start + ii;
+  
+  bf = A (:, idx);
+  R = reshape (bf, dataDim, patchSize, patchSize);
+  shaped = permute (R, [3 2 1]); % x, y, channel
+ 
+  for n = 1:dataDim
+    curAxis = (ii - 1) * dataDim + n;
+    set (gcf, 'CurrentAxes', ha(curAxis));
+    hold on;
+    %%axis image;
+    axis off;
+    axis equal;
+    colormap ('gray')
+    imagesc (shaped (:, :, n));
+    cap = sprintf ('%d [%s]', idx, char (lblChan{n}));
+    text (0.75, 1, cap, 'Color', 'm');
+  end
+  
+  
+end
+
+
+end
