e369be6c3a608665d2bf8e668a36a3988ef129f3
Christian Kellner <gicmo@gnome.org>
2011-07-14 16:17:36 +0200

    Move code from fitModel and make saving the state optional

--- fitModel.m
+++ fitModel.m
@@ -1,27 +1,74 @@
 function [ Model, Results ] = fitModel (modelId)
 
+% local config TODO: move to config file
+shouldSaveState = 0;
+
+% basic init
 clear Model fitPar dispPar Result;
 
 if exist ('state', 'dir') == 0
    mkdir ('state'); 
 end
 
+start = 1;
 tStart = tic;
 
-%addpath (genpath ('functions'));
 figure(1)
 figure(2)
 figure(3)
 
-[ Model, FitParam, DisplayParam, DataParam ] = loadConfig (modelId);
+[Model, fitPar, dispPar, dataPar] = loadConfig (modelId);
 
-if FitParam.startIter <= 1
-  %Model.A = eye(size(Model.A));		% again, we assume A is square
+%% Prepare image data
+Result.images = prepare_images (dataPar);
+
+% Present the filtered pictures (inkluding the excluded patches)
+% to the user for visual validation
+if dataPar.doDebug
+    displayImages (Result.images, dataPar, 1);
 end
 
-% infer matrix
+%% Infer matrix
 %
-[Model, Results] = fitModel_color_plain(Model, FitParam, DisplayParam, DataParam);
+
+[L,M] = size(Model.A);
+dA = zeros(size(Model.A));
+Result.priorN = 0;
+Result.dataIdx = 1;
+Result.X = [];		% force new dataset to be generated
+
+for i = start : fitPar.maxIters
+  Result.iter = i;
+
+  Result = samplePats_plain(Result, fitPar, dataPar);
+
+  if start == 1 && Result.iter == start
+    Model = rescaleBfs(Model, Result);
+  end
+
+  Result.S = pinv(Model.A)*Result.D;
+
+  [Model, Result] = adaptPrior(Model, Result, fitPar);
+
+  if (i == start)
+      Result = updateDisplay_color(Model, Result, fitPar, dispPar, 'init');
+  elseif (rem(i, dispPar.updateFreq) == 0 || i == fitPar.maxIters)
+      Result = updateDisplay_color(Model, Result, fitPar, dispPar);
+  end
+
+  dA = calcDeltaA(Result.S, Model);
+  epsilon = interpIter(i, fitPar.iterPts, fitPar.epsilon);
+  epsilon = epsilon/max(abs(dA(:)));
+  dA = epsilon*dA;
+  Model.A = Model.A + dA;
+
+  if (shouldSaveState && (rem(i, fitPar.saveFreq) == 0 || ...
+      i == fitPar.maxIters))
+    saveState(Model, Result, fitPar);
+  end
+end
+
+Result.tEnd = toc (Result.tStart);
 
 % time reporting
 telapsed = toc (tStart);
