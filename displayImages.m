b40a1a2f43f56d32353e38b1dc681347265f71e3
Christian Kellner <gicmo@gnome.org>
2011-07-18 19:28:43 +0200

    Add caption to images (filtered images)

--- displayImages.m
+++ displayImages.m
@@ -19,6 +19,8 @@ n_plots = ny * nx;
 
 refxs = [1, 3, 3, 1, 1];
 refys = [2, 2, 4, 4, 2];
+lblChan = {{'S (on)'}, {'S (off)'}, {'M (on)'}, {'M (off)'}, {'L (on)'}, {'L (off)'}};
+
 
 for idx=1:n_plots
   n_img = floor ((idx - 1) / ny) + 1;
@@ -32,6 +34,8 @@ for idx=1:n_plots
   axis image;
   axis off;
   fk = plot (refkoos(refxs), refkoos(refys), 'r-');
+  cap = sprintf ('%s', char (lblChan{n_p}));
+  text (5, 15, cap, 'Color', 'r');
   rotate (fi, [0 0 1], 180);
   rotate (fk, [0 0 1], 180);
 end
