c932e2c4f3f6fe6e612a94aacdc69db397c47dff
Christian Kellner <gicmo@gnome.org>
2011-07-19 22:15:19 +0200

    Update CSRectToLMS to take the to be select cols as argument
    one can also pass in shortcuts for, see the file for more info

--- CSRecToLMS.m
+++ CSRecToLMS.m
@@ -1,24 +1,29 @@
-function [ out ] = CSRecToLMS (Model)
+function [ out ] = CSRecToLMS (Model, cols)
 
 out = Model;
 
-C = num2cell (Model.A, 1);
+if length (cols) == 1
+               %  S    M    L 
+   m = [2,4,6; % on,  on,  on
+        1,3,5; % off, off, off
+        2,3,6; % on,  off, on
+        1,4,5  % on,  off, on
+        ];
+   cols = m(cols, :);
+end
 
-C = cellfun (@selectOn, C, 'UniformOutput', false);
+C = num2cell (Model.A, 1);
+selChan = @(X) selectChannel (X, cols);
+C = cellfun (selChan, C, 'UniformOutput', false);
 out.A = cell2mat (C);
 out.dataDim = 3;
 
 end
 
-function [out] = selectOn (Abf)
+function [out] = selectChannel (Abf, cols)
 
 shaped = reshape (Abf, 6, 7*7);
-%X = shaped([2,4,6], :); % Son, Mon, Lon
-%X = shaped([1,3,5], :); % Soff, Moff, Loff
-%X = shaped([2,3,6], :); % Son, Moff, Lon
-X = shaped([1,4,5], :); % Son, Moff, Lon
-
+X = shaped(cols, :); 
 out = reshape (X, 3*7*7, 1);
-
 
 end
