e4458d988ccd059675f2433a758d2216f723e84e
Christian Kellner <gicmo@gnome.org>
2011-07-20 20:46:30 +0200

    Use GPU (matlab parallel programming toolbox) inside calcDeltaA! HACK!

--- functions/calcDeltaA.m
+++ functions/calcDeltaA.m
@@ -10,8 +10,9 @@ A = Model.A;
 npats = size(S,2);
 mp = Model.prior;
 
-dA = zeros(size(A));
-Z  = zeros(size(S));
+%dA = zeros(size(A));
+%Z  = zeros(size(S));
+Z = parallel.gpu.GPUArray.zeros(size(S));
 
 for m=1:M
   s = S(m,:) - mp.mu(m);
@@ -20,7 +21,36 @@ for m=1:M
   Z(m,:) = -(q*c/(mp.sigma(m)^q)) * abs(s).^(q-1) .* sign(s);
 end
 
-dA = -A*Z*S' - npats*A;
+%tM = tic;
+gA = gpuArray (A);
+gZ = gpuArray (Z);
+gS = gpuArray (S);
+gnpats = gpuArray (npats);
 
+gdA = -gA*gZ*gS' - gnpats*gA;
+gdA = gdA/gnpats;
+dA = gather (gdA);
+
+% gA = GPUdouble (A);
+% gZ = GPUdouble (Z);
+% gS = GPUdouble (S);
+% gnpats = GPUdouble (npats);
+% 
+% gdA = -gA*gZ*gS' - gnpats*gA;
+% gdA = gdA/gnpats;
+% dA = double (gdA);
+
+%dA = -A*Z*S' - npats*A;
 % normalize by the number of patterns
-dA = dA/npats;
+%dA = dA/npats;
+
+%te = toc (tM);
+%fprintf ('dA done in %f (gpuArray)\n', te);
+
+
+%tM = tic;
+%dA = -A*Z*S' - npats*A;
+% normalize by the number of patterns
+%dA = dA/npats;
+%te = toc (tM);
+%printf ('dA done in %f (CPU)\n', te);
