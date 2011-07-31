f1e5261d340c50ce2092de1325a9ff91b5a8c752
Christian Kellner <gicmo@gnome.org>
2011-07-31 12:01:35 +0200

    Clean up calcDeltaA

--- functions/calcDeltaA.m
+++ functions/calcDeltaA.m
@@ -6,19 +6,16 @@ function dA = calcDeltaA(S,Model)
 %
 
 A = Model.A;
-[L,M] = size(A);
+[~,M] = size(A);
 npats = size(S,2);
 mp = Model.prior;
-
-%dA = zeros(size(A));
-%Z  = zeros(size(S));
-Z = parallel.gpu.GPUArray.zeros(size(S));
+Z = zeros (size(S));
 
 for m=1:M
   s = S(m,:) - mp.mu(m);
-  q = 2/(1+mp.beta(m));
-  c = (gamma(3/q)/gamma(1/q))^(q/2);
-  Z(m,:) = -(q*c/(mp.sigma(m)^q)) * abs(s).^(q-1) .* sign(s);
+  q = (2/(1+mp.beta(m)));
+  c = (gamma(3/q)/gamma(1/q)).^(q/2);
+  Z(m,:) = -(q*c/(mp.sigma(m).^q)) * abs(s).^(q-1) .* sign(s);
 end
 
 %tM = tic;
@@ -31,26 +28,8 @@ gdA = -gA*gZ*gS' - gnpats*gA;
 gdA = gdA/gnpats;
 dA = gather (gdA);
 
-% gA = GPUdouble (A);
-% gZ = GPUdouble (Z);
-% gS = GPUdouble (S);
-% gnpats = GPUdouble (npats);
-% 
-% gdA = -gA*gZ*gS' - gnpats*gA;
-% gdA = gdA/gnpats;
-% dA = double (gdA);
-
-%dA = -A*Z*S' - npats*A;
-% normalize by the number of patterns
-%dA = dA/npats;
-
-%te = toc (tM);
-%fprintf ('dA done in %f (gpuArray)\n', te);
-
-
-%tM = tic;
-%dA = -A*Z*S' - npats*A;
-% normalize by the number of patterns
-%dA = dA/npats;
 %te = toc (tM);
 %printf ('dA done in %f (CPU)\n', te);
+
+
+end
