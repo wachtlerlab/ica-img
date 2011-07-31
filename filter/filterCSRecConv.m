a07198dbf5784267f3dd0da28c2647caebb69dc4
Christian Kellner <gicmo@gnome.org>
2011-07-31 23:06:37 +0200

    Add possibility to select specific channels inside the filter

--- filter/filterCSRecConv.m
+++ filter/filterCSRecConv.m
@@ -1,9 +1,9 @@
-function [ data ] = filterCSRecConv (Img, dataPar)
+function [ dataFiltered ] = filterCSRecConv (Img, dataPar)
 
 ft = dataPar.filter;
 
-sur = ft;
-sur(2,2) = 0;
+%sur = ft;
+%sur(2,2) = 0;
 %surs = sum (sur(:));
 %fprintf ('Filter: S: %f, C: %f; %f\n', surs, ft(2,2), ft(2,2)/abs (surs));
 
@@ -12,19 +12,23 @@ sur(2,2) = 0;
 wc = ft(2,2);
 ft(2,2) = 0;
 
-input = permute (Img.SML, [3 2 1]);
+%SML is 256x256x3 (r,c,f) wysiwyg :-> 3x256x256 (f,c,r)
+
+input = permute (Img.SML, [3 2 1]); 
 
 % normalize S to the same std as LM
 stds = std (input(1,:));
 stdml = (std (input(2,:)) + std (input(3,:))) * 0.5;
 input(1,:,:) = (stdml / stds) * input(1,:,:);
 
+% (f,c,r) -> (r,c,f)
 input = permute (input, [3 2 1]);
 
 Mc = doConv (input(:,:,2), ft);
 Lc = doConv (input(:,:,3), ft);
 
 LM = ((Lc + Mc) * 0.5);
+
 
 for n=1:3
   % the actual 'filtering'
@@ -34,7 +38,19 @@ for n=1:3
   data(n*2,:,:) = off;
 end
 
+% (f,r,c) -> (f,c,r)
 data = permute (data, [1 3 2]);
+
+if isfield(dataPar, 'activeChs')
+  activeChs = dataPar.activeChs;
+else
+  [nChans,~,~] = size (data);
+  activeChs = 1:nChans; % Use all available channels
+end
+
+% select channels now
+dataFiltered = data(activeChs,:,:);
+
 fprintf ('\t stats after filtering: Min: %f, Max: %f,\n\t\t Mean: %f, Std: %f \n', ...
   min (data(:)), max (data(:)), mean (data(:)), std (data(:)));
 end
