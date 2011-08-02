4f8ba28836e6c0e6a9d95b1a5070a79673782d19
Christian Kellner <gicmo@gnome.org>
2011-08-02 18:21:50 +0200

    Make checkFilter() take an id for the config file

--- checkFilter.m
+++ checkFilter.m
@@ -1,9 +1,18 @@
-modelId = 'color_cs_rect_1';
+function checkFilter (modelId)
+
+if nargin < 1
+modelId = 'color_cs_rect';
+end
+
+fprintf ('Checking filter for [%s]\n', modelId);
 
 clear Model fitPar dispPar Result;
 
 [ Model, FitParam, DisplayParam, DataParam ] = loadConfig (modelId);
+
+fprintf ('Config Id: %s\n', Model.cfgId(1:7));
+
 images = prepare_images (DataParam);
 displayImages (images, DataParam);
 
-
+end
