098bb5a8e1a4e7f2a3795a58bb2bca9012a84c75
Christian Kellner <gicmo@gnome.org>
2011-08-02 18:29:21 +0200

    genFullfigs: Generate a full set of figures of a Model

---
+++ report/genFullfigs.m
@@ -0,0 +1,58 @@
+function genFullfigs (Model, name)
+
+Model = sortModelA (Model);
+filePath = fullfile ('..', 'results', name);
+
+fprintf ('Generating figures for %s (%s)\n\t [saveing to %s]\n',...
+  Model.name, Model.id(1:7), filePath);
+
+if Model.dataDim == 6
+  for n = 1:4
+    Mx = CSRecToLMS (Model, n);
+    plotBfAndAxis (Mx, filePath);
+  end
+else
+  plotBfAndAxis (Model, filePath);
+end
+
+[~,M] = size (Model.A);
+stepSize = 20;
+r = rem (M, stepSize);
+
+for start = 1:stepSize:(M - r)
+  ppBfSingle (Model, start, stepSize, filePath);
+end
+
+ppBfSingle (Model, M-r, r, filePath);
+
+end
+
+
+function [hf] = genFig ()
+
+  hf = figure ('Position', [0, 0, 800, 1000], 'Color', 'w', 'PaperType', 'A4');
+end
+
+
+function plotBfAndAxis (Mx, filePath)
+  hf = genFig ();
+  plotAbf (Mx, hf);
+  do_print (hf, filePath);
+  close (hf);
+  
+  hf = genFig ();
+  plotAxis (Mx, hf);
+  do_print (hf, filePath);
+  close (hf);
+end
+
+function ppBfSingle (Model, start, num, filePath)
+  hf = genFig ();
+  plotABfCSRect (Model, start, num, hf);
+  do_print (hf, filePath);
+  close (hf);
+end
+
+function do_print (hf, filePath)
+  print(hf, '-dpsc2', '-append', filePath);
+end
