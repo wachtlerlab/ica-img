21b6cac2b1897d24c346ba3a4982cfc8bbfb07e6
Christian Kellner <gicmo@gnome.org>
2011-08-03 13:16:02 +0200

    Use the new chan2str function to get the string repr. of channels

--- plotABfCSRect.m
+++ plotABfCSRect.m
@@ -26,10 +26,16 @@ A = Model.A;
 A = normABf (A);
 ha = tight_subplot (num, dataDim*2, [.001 .02], 0);
 
-if dataDim == 6
-  lblChan = {{'S|on'}, {'S|off'}, {'M|on'}, {'M|off'}, {'L|on'}, {'L|off'}};
+if isfield(Model, 'chanMap')
+    chanMap = Model.chanMap;
 else
-  lblChan = {{'S'}, {'M'}, {'L'}};
+    if dataDim == 6
+     chanMap = [49 17 50 18 51 19];
+    elseif dataDim == 3
+    chanMap = [1 2 3];
+    elseif dataDim == 4
+    chanMap = [0 0 0 0];
+    end
 end
 
 
@@ -63,7 +69,7 @@ for ii = 1:num
 
     colormap ('gray')
     image (wp);
-    cap = sprintf ('%3d %s', idx, char (lblChan{n}));
+    cap = sprintf ('%3d %s', idx, chan2str (n, chanMap));
     text (1, 0.8*M, cap, 'Color', 'black', 'FontSize', 8);
     
     xcr = mod(n,dataDim)+1;
@@ -72,9 +78,9 @@ for ii = 1:num
     xcr2 = mod(n+1,dataDim)+1;
     cr2 =  do_corr (R(n,:), R (xcr2,:));
     
-    txt = sprintf ('%.2f %s', cr, char (lblChan{xcr}));
+    txt = sprintf ('%.2f %s', cr, chan2str (xcr, chanMap));
     text (1, (M/2)+10, txt, 'Color', 'red', 'FontSize', 5);
-    txt = sprintf ('%.2f %s', cr2, char (lblChan{xcr2}));
+    txt = sprintf ('%.2f %s', cr2, chan2str (xcr2, chanMap));
     text (1, (0.2*M), txt, 'Color', 'blue', 'FontSize', 5);
     
   end
@@ -84,7 +90,7 @@ end
 
 
 function [out] = do_corr (a, b)
-
 out = sum (a.*b);
 %out = max (xcorr (a, b, 'coeff'));
 end
+
