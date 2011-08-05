c10f41a0778f07de19707b1cbd68c8fa41179b73
Christian Kellner <gicmo@gnome.org>
2011-08-05 22:38:54 +0200

    Use new chanMap code

--- displayImages.m
+++ displayImages.m
@@ -12,42 +12,133 @@ set (figNum, 'Color', [1 1 1]);
 colormap (gray)
 
 nx = length (images);
-ny = dataPar.dataDim;
+ny = dataPar.dataDim + 2;
 ha = tight_subplot (nx, ny, [.01 .03], [.01 .01]);
-
-n_plots = ny * nx;
 
 refxs = [1, 3, 3, 1, 1];
 refys = [2, 2, 4, 4, 2];
-lblChan = {{'S (on)'}, {'S (off)'}, {'M (on)'}, {'M (off)'}, {'L (on)'}, {'L (off)'}};
 
+if isfield(dataPar, 'chanMap')
+  chanMap = dataPar.chanMap;
+else
+  dataDim = dataPar.dataDim;
+  if dataDim == 6
+    chanMap = [49 17 50 18 51 19];
+  elseif dataDim == 3
+    chanMap = [1 2 3];
+  elseif dataDim == 4
+    chanMap = [0 0 0 0];
+  end
+end
 
-for idx=1:n_plots
-  n_img = floor ((idx - 1) / ny) + 1;
-  n_p = mod (idx  - 1, ny) + 1;
-  img = images(n_img).imgData;
-  refkoos = images(n_img).refkoos;
-  bf = squeeze (img(n_p, :, :));
-  set (gcf, 'CurrentAxes', ha(idx));
-  hold on
-  fi = imagesc(bf');
-  axis image;
-  axis off;
-  fk = plot (refkoos(refxs), refkoos(refys), 'r-');
+for idx=1:nx
+  fprintf ('\nImage %s\n', images(idx).filename);
+  curImg = images(idx);
+  img = getImgData (curImg, 0);
+  d = img(:);
+  fprintf ('min: %.5f max: %.5f std: %.5f\n', ...
+    min (d), max (d), std (d));
+  img = getImgData (curImg, 0);
+  d = img(:);
+  fprintf ('min: %.5f max: %.5f std: %.5f\n', ...
+    min (d), max (d), std (d));
+  refkoos = images(idx).refkoos;
   
-  % if we selected channels adjust n_p
-  if isfield(dataPar, 'activeChs')
-    activeChs = dataPar.activeChs;
-    n_p = activeChs(n_p);
+  for n = 1:(ny - 2)
+      
+    cur_plot = (idx - 1) * ny + n;
+
+    chan = squeeze (img(n, :, :));
+    set (gcf, 'CurrentAxes', ha(cur_plot));
+    hold on
+    fi = imagesc(chan');
+    axis image;
+    axis off;
+    fk = plot (refkoos(refxs), refkoos(refys), 'r-');
+  
+    n_img = n;
+    
+    fprintf ('\t [%s] min: %.5f max: %.5f std: %.5f\n', ...
+      chan2str (n_img, chanMap),...
+      min(chan(:)), max(chan(:)), std(chan(:)));
+    
+    text (5, 15, chan2str (n_img, chanMap), 'Color', 'r');
+    rotate (fi, [0 0 1], 180);
+    rotate (fk, [0 0 1], 180);
   end
   
-  cap = sprintf ('%s', char (lblChan{n_p}));
-  text (5, 15, cap, 'Color', 'r');
-  rotate (fi, [0 0 1], 180);
-  rotate (fk, [0 0 1], 180);
+  if dataPar.dataDim == 6
+    set (gcf, 'CurrentAxes', ha((idx - 1) * ny + (ny - 1)));
+    d = getImgData (curImg, 1, [5 3 1]);
+    p = permute (d, [3 2 1]);
+    imagesc (p);
+    axis off;
+  
+    set (gcf, 'CurrentAxes', ha((idx - 1) * ny + ny));
+    d = getImgData (curImg, 1, [6 4 2]);
+    p = permute (d, [3 2 1]);
+    imagesc (p);
+    axis off;
+  elseif dataPar.dataDim == 4
+    s = size (curImg);
+    set (gcf, 'CurrentAxes', ha((idx - 1) * ny + (ny - 1)));
+    d = getImgData (curImg, 1, [3 1]);
+    d(3,:,:) = zeros (s(2), s(2));
+    p = permute (d, [3 2 1]); 
+    imagesc (p);
+    axis off;
+    
+    set (gcf, 'CurrentAxes', ha((idx - 1) * ny + ny));
+    d = getImgData (curImg, 1, [4 2]);
+    d(3,:,:) = zeros (s(2), s(2));
+    p = permute (d, [3 2 1]);
+    imagesc (p);
+    axis off;
+    
+  elseif dataPar.dataDim == 3
+    s = size (curImg);
+    set (gcf, 'CurrentAxes', ha((idx - 1) * ny + ny));
+    d = getImgData (curImg, 1);
+    p = permute (d, [3 2 1]);
+    %p(3,:,:) = zeros (s(2), s(2));
+    imagesc (p);
+    axis off;
+    
+  end
 end
 
 drawnow;
+
+end
+
+function [data] = getImgData (img, doNorm, channels)
+data = img.imgData;
+
+if doNorm
+  C = num2cell (data, 1);
+  C = cellfun (@normA, C, 'UniformOutput', false);
+  data = cell2mat (C);
+  
+  if nargin > 2
+    data = data(channels, :,:);
+  end
+  
+end
+
+end
+
+
+function [normed] = normA (A)
+A = A - min (A);
+normed =  A / max(abs(A(:)));
+%normed = normed * 255;
+end
+
+function [out] = selectChannel (Abf, cols, l, dataDim)
+
+shaped = reshape (Abf, dataDim, l*l);
+X = shaped(cols, :); 
+out = reshape (X, length(cols)*l, 1);
 
 end
 
