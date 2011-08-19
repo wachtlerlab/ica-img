95645bc9fbd068209c1c2353947db81a6edfb2a7
Christian Kellner <gicmo@gnome.org>
2011-08-19 15:38:29 +0200

    Fix the p2Size parameter in the absmax_setup code. oh oh. ;-)

--- cuda/absmax_setup.m
+++ cuda/absmax_setup.m
@@ -3,7 +3,7 @@ function [ gpuContext ] = absmax_setup (A)
 k_absmax = parallel.gpu.CUDAKernel ('absmax_kernel.ptx',...
                                     'absmax_kernel.cu');                                  
 gpuContext.kernel = k_absmax;
-dataSize = length (A)*2;
+dataSize = length (A)^2;
 gpuContext.dataSize = dataSize;
 p2Size = pow2 (nextpow2 (dataSize));
 gpuContext.p2Size = p2Size;
