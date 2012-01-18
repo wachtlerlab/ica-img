0a520fe8ca3ae8d32d78cb05146556a489d30431
Christian Kellner <kellner@biologie.uni-muenchen.de>
2012-01-18 18:35:26 +0100

    Mexcian hat function (and slight changes to DoG)

--- filter/dog.m
+++ filter/dog.m
@@ -1,12 +1,14 @@
-function [ y ] = dog (x, mu, s_i, s_o)
+function [ y ] = dog (x, mu, s_i, s_o, ratio)
 %DOG Differance of Gaussions
 
-y = do_gauss (x, mu, s_i) - do_gauss (x, mu, s_o);
+if nargin < 5
+  ratio = 1.0;
+end
+
+y = do_gauss (x, mu, s_i) - ratio * do_gauss (x, mu, s_o);
 
 end
 
 function [out] = do_gauss (x, mu, sigma)
-
 out = 1/(2*pi*sigma^2)*exp(-(x - mu).^2/(2*sigma^2));
-
 end
