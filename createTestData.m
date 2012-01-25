0ae5d8b882d585fc707c1c4385482810858f936b
Christian Kellner <gicmo@gnome.org>
2012-01-25 22:16:43 +0100

    TestData: include dataset

--- createTestData.m
+++ createTestData.m
@@ -17,6 +17,7 @@ for n = 1:N
   data(n, 2) = interpIter (i, iterPts, epsSteps);
 end
 
+%% ---- interpolate test ----
 gcpl = H5P.create ('H5P_LINK_CREATE');
 H5P.set_create_intermediate_group (gcpl, 1);
 gt = '/test/interpolate';
@@ -36,6 +37,29 @@ gt = '/test/pinv';
 group = H5G.create (fd, gt, gcpl,'H5P_DEFAULT', 'H5P_DEFAULT');
 saveData (fd, [gt '/A'], A);
 saveData (fd, [gt '/Ai'], Ai);
+
+H5G.close (group);
+
+%% ---- dataset ---
+ds = createDataSet ('tri_csr_0_log');
+
+gcpl = H5P.create ('H5P_LINK_CREATE');
+H5P.set_create_intermediate_group (gcpl, 1);
+group = H5G.create (fd, '/test/dataset', gcpl,'H5P_DEFAULT', 'H5P_DEFAULT');
+
+writeAttrInt (group, 'patchsize', ds.patchsize);
+writeAttrInt (group, 'npats', ds.npats);
+writeAttrInt (group, 'blocksize', ds.blocksize);
+writeAttrInt (group, 'nclusters', ds.nclusters);
+
+
+saveData (fd, '/test/dataset/indicies', ds.indicies);
+saveData (fd, '/test/dataset/patsperm', ds.patsperm);
+saveData (fd, '/test/dataset/imgdata', ds.imgdata);
+saveData (fd, '/test/dataset/Ainit', ds.Ainit);
+
+c0 = extractPatches(ds, 1);
+saveData (fd, '/test/dataset/c0', c0);
 
 H5G.close (group);
 H5F.close (fd);
@@ -60,5 +84,16 @@ end
 
 H5S.close(dsp);
 H5T.close(dtype);
+
+end
+
+function [dsid] = writeAttrInt (loc, name, value)
+
+space = H5S.create('H5S_SCALAR');
+attr = H5A.create (loc, name, 'H5T_NATIVE_INT', space, 'H5P_DEFAULT');
+H5A.write (attr, 'H5T_NATIVE_INT32', int32 (value(1)));
+H5A.close (attr);
+H5S.close (space);
+
 
 end
