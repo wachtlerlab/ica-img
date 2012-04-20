d575f4f41628e112423e7d328b247b42c91f8d83
Christian Kellner <gicmo@gnome.org>
2012-04-20 16:02:10 +0200

    str2chan: Recognize ['+', '-'] channel qualifiers

--- utils/str2chan.m
+++ utils/str2chan.m
@@ -1,28 +1,30 @@
 function [id] = str2chan (chan, onoff)
 
-if ischar(chan)
-    
-    if strcmpi (chan, 'S')
-        chanNum = 1;
-    elseif strcmpi (chan, 'M')
-        chanNum = 2;
-    elseif strcmpi (chan, 'L')
-        chanNum = 3;
-    else
-        error ('Unkown input!');
+if nargin < 2
+  onoff = '';
+end
+
+if ischar(chan(1))
+    chanNum = char2chan(chan(1)); 
+    if (length (chan) > 1)
+      onoff = chan(2:end);
     end
 else
     chanNum = chan;
 end
 
+if chanNum < 1 || chanNum > 3
+  warning ('ICA:invalid_channel_id', 'Invalid channel id')
+end
+
 id = chanNum;
 
-if nargin > 1
-    
+if ~isempty(onoff) 
+  
     if ischar(onoff)
-       if strcmpi (onoff, 'on')
+       if strcmpi (onoff, 'on') || strcmpi (onoff, '+') 
             onoff = 1;
-       elseif strcmpi (onoff, 'off')
+       elseif strcmpi (onoff, 'off') || strcmpi (onoff, '-') 
            onoff = 0;
        else
            error ('Unkown onoff input');
@@ -31,6 +33,19 @@ if nargin > 1
     
     id = bitor (id, bin2dec ('00010000'));
     id = bitor (id, bitshift (onoff, 5));
+end
+
+end
+
+function [chanNum] = char2chan (ch)
+if strcmpi (ch, 'S')
+  chanNum = 1;
+elseif strcmpi (ch, 'M')
+  chanNum = 2;
+elseif strcmpi (ch, 'L')
+  chanNum = 3;
+else
+  error ('Unkown input!');
 end
 
 end
