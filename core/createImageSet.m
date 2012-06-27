f624f89ed4238f74f43b4f8f273162909302eeb8
Christian Kellner <kellner@biologie.uni-muenchen.de>
2012-06-27 15:29:31 +0200

    createImageSet: restructure and use the filter.setup() function if available

--- core/createImageSet.m
+++ core/createImageSet.m
@@ -5,39 +5,40 @@ source = cfg.source;
 % compat hack
 dataDir = fullfile ('..', 'data', source.database);
 
-
-if isfield (cfg, 'filter')
-  fcfg = cfg.filter;
-  
-  filterFunc = [fcfg.class];
-  filter = feval (filterFunc, fcfg);
-else
-  filter = [];
-end
-
 nimages = length(source.images);
 images = cell(nimages, 1);
 
 for n=1:nimages
   filename = source.images{n};
-  img = loadImage (filename, dataDir);
-  
-  if isstruct(filter)
-    img  = filter.function (filter, img);
-  end
-  
-  images{n} = img;
+  images{n} = loadImage (filename, dataDir);  
 end
 
 imageset.database = source.database;
+
+% filtering 
+if isfield (cfg, 'filter')
+  fcfg = cfg.filter;
+  
+  filterFunc = [fcfg.class];
+  filter = feval (filterFunc, fcfg);
+  
+  if isfield (filter, 'setup')
+    filter = filter.setup (filter, images);
+  end
+  
+  for n=1:nimages
+    fprintf ('Filtering [%s]\n', images{n}.filename);
+    images{n} = filter.function (filter, images{n});
+  end
+  
+  imageset.channels = uint8(filter.channels);
+end
+
 imageset.images = images;
 imageset.shape = [size(images{1}.data), length(images)];
 
 %shape is [z, x, y, n] with z begin the channel, x, y the image axis and
 %      n the number of images
-if isstruct(filter)
-  imageset.channels = uint8(filter.channels);
-end
 
 end
 
