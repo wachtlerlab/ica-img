ba771a5bd413ec2ea76519114c021e8c96e9c501
Christian Kellner <kellner@biologie.uni-muenchen.de>
2012-07-31 14:50:47 +0200

    checkImages: function to look at the images

---
+++ plot/checkImages.m
@@ -0,0 +1,30 @@
+function checkImages (cfg)
+
+if ischar(cfg)
+  cfg = loadConfig (cfg);
+end
+
+imgset = createImageSet (cfg.data);
+
+displayImages(imgset);
+
+% figure()
+% load ('brcmap');
+% for n=1:length(imgset.images)
+%   
+%   img = imgset.images{n};
+%   data = img.data;
+%   x = reshape (data, shape(1), shape(2) * shape(3));
+%   C = corrcoef (x')
+%   subplot(2,length(imgset.images),n)
+%   imagesc(C)
+%   colormap(cool)
+%   
+%   C = Whitening(img, 5)
+%   subplot(2,length(imgset.images),length(imgset.images)+n)
+%   imshow(C);
+%   colormap(cool)
+% end
+
+end
+
