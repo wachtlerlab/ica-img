d3f609cccc35a12fd0eeac751909e37270e80625
Christian Kellner <ck@xatom.net>
2012-11-28 13:20:17 +0100

    mapChannels: take rectify parameter

--- utils/mapChannel.m
+++ utils/mapChannel.m
@@ -1,12 +1,27 @@
-function [channels] = mapChannel (chanlist)
+function [channels] = mapChannel (chanlist, rectify)
+
+if nargin < 2
+    rectify = 1;
+end
 
 N = length (chanlist);
-channels = zeros (1, N*2);
+
+if rectify
+    M = N*2; 
+else
+    M = N;
+end
+
+channels = zeros (1, M);
 
 for n = 1:N
   ch = chanlist(n);
-  channels(1, (n*2-1)) = str2chan (ch, 'on');
-  channels(1, n*2) = str2chan (ch, 'off');
+  if rectify
+    channels(1, (n*2-1)) = str2chan (ch, 'on');
+    channels(1, n*2) = str2chan (ch, 'off');
+  else
+     channels(1, n) = str2chan (ch); 
+  end
 end
 
 end
