be547649963abf1e51e3a1791dae89f39d40c08e
Christian Kellner <ck@xatom.net>
2013-02-11 09:42:31 +0100

    imgallpatches: add new tiling parameter

--- utils/imgallpatches.m
+++ utils/imgallpatches.m
@@ -1,21 +1,53 @@
-function [data] = imgallpatches(img, patchsize)
+function [data, pos] = imgallpatches(img, patchsize, tiling)
+
+if ~exist('tiling', 'var'); tiling = 1; end
 
 [m, n, c] = size (img);
 
-nm = m - patchsize;
-nn = n - patchsize;
+sm = gen_coords(m, patchsize, tiling);
+sn = gen_coords(n, patchsize, tiling);
 
-cr = repmat (1:nm, 1, nn);
-cc = sort (cr);
+cm = repmat(sm, 1, length(sn));
+cn = sort(repmat(sn, 1, length(sm)));
+
+pos = [cm; cn];
 
 samplesize = patchsize^2 * c;
-npats = length(cc);
+npats = length(pos);
 data = zeros (samplesize, npats);
 
 for k=1:npats
-  patch = img(cr(k):cr(k)+patchsize-1, cc(k):cc(k)+patchsize-1, :);
+  rm = pos(1,k):pos(1,k)+patchsize-1;
+  rn = pos(2,k):pos(2,k)+patchsize-1;
+  
+  patch = img(rm, rn, :);
   data(:,k) = reshape (patch, samplesize, 1);
 end
 
 end
 
+
+function [y] = gen_coords(x, patchsize, tiling)
+
+r = mod(x, tiling) - (patchsize - 1);
+x = x + r * (r < 1);
+y = 1:tiling:x;
+
+end
+
+
+% r = mod(m, tiling) - (patchsize - 1);
+% m = m + r * (r < 1);
+% 
+% r = mod(n, tiling) - (patchsize - 1);
+% n = n + r * (r < 1);
+% 
+% sm = 1:tiling:m;
+% sn = 1:tiling:n;
+
+%r = mod(m, patchsize);
+%m = m - max((patchsize * (tiling == 1)), r);
+%n = n - max((patchsize * (tiling == 1)), r);
+%cr = repmat (1:tiling:m, 1, n/tiling);
+%cc = sort (cr);
+%pos = [cr; cc];
