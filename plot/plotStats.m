9ba8e1196253107da630f6308f74aed51719a42c
Christian Kellner <ck@xatom.net>
2013-02-11 10:02:01 +0100

    plotStats: new function that prints S stats

---
+++ plot/plotStats.m
@@ -0,0 +1,21 @@
+function [ Result ] = plotStats(Model, ds)
+
+%addpath('functions/home/tewon/Matlab/Lewicki/expwrcode/stats')
+%addpath('functions/home/lewicki/matlab/oc')
+Result.iter = (ds.npats / ds.blocksize) * (ds.nclusters-2) + 1;
+Result = samplePats(Result, ds)
+Result.S = pinv(Model.A)*Result.D;
+
+Model.prior.beta = Model.beta;
+Model.prior.sigma = ones(size(Model.beta, 1));
+Model.prior.mu = zeros(size(Model.beta, 1));
+
+figure(2);
+figure(3);
+
+Result.iter = 1;
+updateDisplay (Model, Result, 1);
+
+
+end
+
