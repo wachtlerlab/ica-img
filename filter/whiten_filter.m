8bf3f25fafa3b454862054a857dcf22ee726700e
Christian Kellner <ck@xatom.net>
2013-02-11 09:43:26 +0100

    whiten_filter: cosmetic changes

--- filter/whiten_filter.m
+++ filter/whiten_filter.m
@@ -1,12 +1,15 @@
 function [ W ] = whiten_filter (data)
+%whiten_filter whiten the input
+%   rows of data are observations
+%   (c) Christian Kellner <kellner@bio.lmu.de>
 
-X = bsxfun(@minus, data, mean(data,2));
+X = bsxfun(@minus, data, mean(data, 2));
 
-[~, nelm] = size (data);
+[~, nelm] = size(data);
 C = (X*X')/(nelm-1);
 [E, D] = eig(C);
 
-W = E* diag(1./(diag(D)).^(1/2)) * E';
+W = E * diag(1./(diag(D)).^(1/2)) * E';
 
 end
 
