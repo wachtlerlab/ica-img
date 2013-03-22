a357e03d1fe52e3f6c17cfddb9e81004afa3e2dd
Christian Kellner <christian@kellner.me>
2013-03-22 10:43:07 +0100

    prepareAfromModel: new function to setup A struct

---
+++ plot/prepareAfromModel.m
@@ -0,0 +1,13 @@
+function [ A ] = prepareAfromModel(Model, nchan)
+
+ps = Model.ds.patchsize;
+
+A = struct();
+A.name    = [Model.cfg.id(1:7) '-' Model.id(1:7)];
+A.id      = Model.id;
+A.nchan   = nchan;
+A.nbf     = size(Model.A, 2);
+A.ps      = ps;
+
+end
+
