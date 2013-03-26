23bd1f07a9e4b2891d6719ecc5e6599a3c72c5be
kellner@bio.lmu.de <gicmo@wachtler9.cns.bzm>
2013-03-26 17:32:09 +0100

    filterExpwerFit: [new] filter activations by fitting an exp. power. dist

---
+++ filterExpwerFit.m
@@ -0,0 +1,39 @@
+function [ epp, wfilter ] = filterExpwerFit(wi, border)
+
+if ~exist('border', 'var'); border = 0.2; end;
+
+mu    = mean(wi);
+sigma = std(wi);
+
+beta = expwrmapbeta (wi, mu, sigma, 2, 2, 0.1);
+epp = [mu, sigma, beta];
+
+if (abs(mu) > 0.01) || beta < 0
+    wfilter = ones(size(wi));
+    return;
+end
+
+stwide = 0.05;
+intwidth = 6;
+
+x = (mu-intwidth):stwide:(mu+intwidth);
+intx = @(b) integral(@(x) expwrpdf(x, mu, sigma, beta), -Inf, b);
+yf = arrayfun(intx, x);
+
+idx_a = find(yf > (border*0.5), 1);
+idx_b = find(yf > 1 - (border*0.5), 1);
+
+lowerb = x(idx_a);
+upperb = x(idx_b);
+
+wfilter = wi < lowerb | wi > upperb;
+
+% if nargout > 2
+%     bounds = zeros(2,2);
+%     bounds(1,:) = [yf(idx_a), yf(idx_b)];
+%     bounds(2,:) = [lowerb, upperb];
+% end
+
+
+end
+
