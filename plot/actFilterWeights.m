bdf732261244edc8f45269bfede289b62c738252
kellner@bio.lmu.de <gicmo@wachtler9.cns.bzm>
2013-03-26 17:32:33 +0100

    actFilterWeights: Use new filterEx.. function

--- plot/actFilterWeights.m
+++ plot/actFilterWeights.m
@@ -1,6 +1,9 @@
-function [ Act ] = actFilterWeights(Act)
+function [ Act ] = actFilterWeights(Act, border)
+
+if ~exist('border', 'var'); border = 0.1; end;
 
 nbf = size(Act.w, 2);
+nimg = size(Act.offset, 1);
 offset = Act.offset;
 
 perimg = @(data, idx, func) cell2mat(arrayfun(...
@@ -9,28 +12,50 @@ perimg = @(data, idx, func) cell2mat(arrayfun(...
 
 wfilter = zeros(size(Act.w));
 
-for n=1:nbf
+epp = zeros([nbf, nimg, 3]);
+wall = Act.w;
+
+tic;
+parfor n=1:nbf
     
     fprintf(' %03d', n);
     
-    w = Act.w(:, n);
+    w = wall(:, n);
     
-    for img=1:8
-        irange = offset(img, 1):offset(img, 2);
+    wfbf = zeros(size(w, 1)/nimg, nimg);
+    
+    for img=1:nimg
+        %irange = offset(img, 1):offset(img, 2);
         wi = w(offset(img, 1):offset(img, 2));
-        wc = wi - mean(wi);
-        stdse =  median(abs(wc))/0.6745;
-        wfilter(irange, n) = abs(wc) > 1.5*stdse;
+        
+        %[epp_, wf, bounds] = filterExpwerFit(wi, 0.1);
+        [epp_, wf] = filterExpwerFit(wi, border);
+        epp(n, img, :) = epp_;
+        wfbf(:, img) = wf;
+        %wfilter(irange, n) = wf;
     end
     
+    wfilter(:, n) = wfbf(:);
+    
+%      if any(wfilter(:, n) - wfbf(:))
+%         warning('nonmatch'); 
+%      end  
+%      fprintf('\n\n')
+%     
     fprintf('\b\b\b\b');
 end
 
+Act.epp = epp;
 Act.wfilter = logical(wfilter);
+
+tcalc = toc;
+fprintf('t: %f\n', tcalc);
 
 end
 
-
+        %wc = wi - mean(wi);
+        %stdse =  median(abs(wc))/0.6745;
+        %wfilter(irange, n) = abs(wc) > 1.5*stdse;
 
     %wc = perimg(w, offset, @(x) (x - mean(x))');
     %stdse = perimg(wc, offset, @(x) median(abs(x))/0.6745);
