73d17ecab4192e324da99938acdedd7c35cb2050
Christian Kellner <christian@kellner.me>
2013-06-06 13:02:49 +0200

    calcKLDTable: add mean to the table

--- plot/calcKLDTable.m
+++ plot/calcKLDTable.m
@@ -1,12 +1,13 @@
 function [ table ] = calcKLDTable( Acts )
 
 L = length(Acts);
-table = cell(L, 2);
+table = cell(L, 3);
 for n = 1:L
     act = Acts(n);
     A = AfromActivations(act);
     table{n, 1} = A.name;
     table{n, 2} = calcAklDiv(A);
+    table{n, 3} = mean(A.beta);
 end
 
 end
